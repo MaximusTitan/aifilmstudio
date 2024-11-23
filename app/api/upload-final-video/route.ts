bcac3131583640912db29f863766c95e9a7b586c
Yella Yohan Reddy <yohanreddy1@gmail.com>
2024-11-23 15:32:25 +0530

    Fix: Deployment Error

--- app/api/upload-final-video/route.ts
+++ app/api/upload-final-video/route.ts
@@ -1,14 +1,6 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: '100mb', // Adjust the limit as needed
-    },
-  },
-};
-
 export async function POST(request: Request) {
   const formData = await request.formData();
   const videoFile = formData.get("video") as File;
