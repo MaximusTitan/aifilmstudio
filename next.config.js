af67858e8105f4d18f18a80acca729b90778ed0d
Yella Yohan Reddy <yohanreddy1@gmail.com>
2025-01-22 22:36:59 +0530

    Fix: CORS issue

--- next.config.js
+++ next.config.js
@@ -70,10 +70,6 @@ const nextConfig = {
         source: "/:path*",
         headers: [
           {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "credentialless",
-          },
-          {
             key: "Cross-Origin-Opener-Policy",
             value: "same-origin",
           },
